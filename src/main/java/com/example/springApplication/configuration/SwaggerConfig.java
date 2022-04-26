d4ab70f1d696e7ff6a4f8ee7829db1bbd35c6f65
aysunTagiyeva <aysuntagiyeva@mail.ru>
2022-04-26 12:29:35 +0400

    latest

---
+++ src/main/java/com/example/springApplication/configuration/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.springApplication.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.springApplication"))
+                .paths(PathSelectors.any())
+                .build();
+        /* basePackage - To configure (for itself) APIs in all classes with @Controller or @RestController
+          under this package. It gives us configuration, and we can apply for services. */
+    }
+
+}
